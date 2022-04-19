53758aca58fe51482328e7da71fb821ed5622b04
Liliana1609 <lily.webdev22@gmail.com>
2022-04-19 18:15:31 -0400

    Organização dos Personagens

--- index.js
+++ index.js
@@ -1,54 +1,19 @@
-/**
- * Cria o objeto "Desenhavel" que será a classe base para
- * todos os objetos desenháveis do jogo. Define também as variáveis padrão
- * que todos os objetos filhos herdarão, assim como as funções padrão.
- */
- function Desenhavel() {
-  this.velocidade = 0;
-  this.larguraCanvas = 0;
-  this.alturaCanvas = 0;
+const button = document.querySelector('button');
+const popup = document.querySelector('.popup-wrapper');
 
-  // Define uma função abastrata para ser sobrescrita nos objetos filho
-  this.desenhar = function() {
-  };
+button.addEventListener('click' , () => {
+  popup.style.display = 'block'
+})
 
-  this.iniciar = function(x, y) {
-        // Variáveis padrão do eixo cartesiano
-        this.x = x;
-        this.y = y;
+popup.addEventListener ('click', event => {
+  const classNameOfClikedElement = event.target.classList[0]
+  const classNames = ['popup-close', 'popup-wrapper' , 'popup-link']
+  const shouldClosePopup = classNames.some(className =>
+    className === classNameOfClikedElement)
+
+  if (shouldClosePopup) {
+    popup.style.display = 'none'
   }
-}
-/**
- * Define um objeto para manter todas as nossas imagens do jogo para
- * evitar que elas sejam criadas mais de uma vez.
- */
- var repositorio = new function() {
-      this.planofundo = new Image();
+})
 
-      // Configura os caminhos (src) das imagens
-      this.planofundo.src = "IMAGENS/COMPLETA.JPG";
-}
-/**
- * Cria o objeto PlanoFundo que se tornará um filho do
- * objeto Desenhavel. O plano de fundo será desenhado nesse objeto
- * e criará a ilusão de movimento ao deslocar a imagem.
- */
- function PlanoFundo() {
-      this.velocidade = 1; // Redefine a velocidade do plano de fundo para pintura
 
-      // Implementa a função abstrata
-      this.desenhar = function() {
-            // Pinta o plano de fundo
-            this.x -= this.velocidade;
-            this.context.drawImage(repositorio.planofundo, this.x, this.y);
-
-            // Desenha outra imagem na borda superior da primeira imagem
-            this.context.drawImage(repositorio.planofundo, this.x + this.larguraCanvas, this.y);
-
-            // Se a imagem for deslocada para fora da tela, redefine-a
-            if (Math.abs(this.x) >= this.larguraCanvas)
-                   this.x = 0;
-      };
-}
-// Define o PlanoFundo como herdeiro das propriedades de Desenhavel
-Plano ...
